9d21df431680135c5224ac700b317518117e0a5b
Karan Mundhada <92524719+Karan-mundhada@users.noreply.github.com>
2024-12-04 00:10:23 +0530

    Time: 0 ms (100.00%) | Memory: 9 MB (5.94%) - LeetSync

---
+++ 171-excel-sheet-column-number/excel-sheet-column-number.cpp
@@ -0,0 +1,13 @@
+class Solution {
+public:
+    int titleToNumber(string columnTitle) {
+        int ans = 0;
+        int n = columnTitle.length();
+        for(int i = n-1; i>=0; i--)
+        {
+            ans += pow(26, n - 1 - i) * (columnTitle[i] - 'A' + 1);
+        }
+
+        return ans;
+    }
+};
